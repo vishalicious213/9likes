8893f4af75e0e3656872bb2b5fbcc582b9587a58
vishalicious213 <vishalicious213@gmail.com>
2023-04-12 10:39:33 -0700

    drafted data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+const cats = [
+    {
+        name: "Mika",
+        avatar: "images/cat-mika.jpg",
+        age: 5,
+        bio: "What's mine is mine, and what's yours is mine.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Miko",
+        avatar: "images/cat-miko.jpg",
+        age: 2,
+        bio: "Why can't they call me the ADORABLE snow man?!!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Zdana",
+        avatar: "images/cat-zdana.jpg",
+        age: 7,
+        bio: "From Russia with love.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    }
+]
