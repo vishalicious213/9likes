e3bbca5a57deae43b36083127b8b29d854c0d2a2
vishalicious213 <vishalicious213@gmail.com>
2023-04-12 12:11:14 -0700

    renderCat() renders hard-coded cat avatar

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import cats from "./data.js"
 import Cat from "./cat.js"
 
-console.log("9likes")
+const catSection = document.getElementById("cat")
+
+// ⬇️ RENDER THE APP ⬇️
+
+function renderCat() {
+    catSection.innerHTML = ""
+    catSection.style.backgroundImage = `url(${cats[0].avatar})`
+
+    catSection.innerHTML += `
+        <div class="cat-profile">
+            <div class="profile-img"></div>
+        </div>
+    `
+}
+
+renderCat()
