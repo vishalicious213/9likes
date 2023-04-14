772a68d7efe33171b3d9946e4d0b81e3e2755f7e
vishalicious213 <vishalicious213@gmail.com>
2023-04-13 23:16:49 -0700

    moved cat images into /images/cats folder and updated data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const cats = [
     {
         name: "Mika",
-        avatar: "images/cat-mika.jpg",
+        avatar: "images/cats/cat-mika.jpg",
         age: 5,
         bio: "What's mine is mine, and what's yours is mine.",
         hasBeenSwiped: false,
@@ -9,7 +9,7 @@ const cats = [
     },
     {
         name: "Miko",
-        avatar: "images/cat-miko.jpg",
+        avatar: "images/cats/cat-miko.jpg",
         age: 2,
         bio: "Why can't they call me the ADORABLE snow man?!!",
         hasBeenSwiped: false,
@@ -17,7 +17,7 @@ const cats = [
     },
     {
         name: "Zdana",
-        avatar: "images/cat-zdana.jpg",
+        avatar: "images/cats/cat-zdana.jpg",
         age: 7,
         bio: "From Russia with love.",
         hasBeenSwiped: false,
