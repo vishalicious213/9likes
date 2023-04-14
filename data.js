37477c06785fdc6711db2ecf3beef65ec0c315c9
vishalicious213 <vishalicious213@gmail.com>
2023-04-13 23:18:06 -0700

    updated data.js with cat breeds

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ const cats = [
         name: "Mika",
         avatar: "images/cats/cat-mika.jpg",
         age: 5,
+        breed: "Siberian",
         bio: "What's mine is mine, and what's yours is mine.",
         hasBeenSwiped: false,
         hasBeenLiked: false
@@ -11,6 +12,7 @@ const cats = [
         name: "Miko",
         avatar: "images/cats/cat-miko.jpg",
         age: 2,
+        breed: "Himalayan",
         bio: "Why can't they call me the ADORABLE snow man?!!",
         hasBeenSwiped: false,
         hasBeenLiked: false
@@ -19,6 +21,7 @@ const cats = [
         name: "Zdana",
         avatar: "images/cats/cat-zdana.jpg",
         age: 7,
+        breed: "Siberian",
         bio: "From Russia with love.",
         hasBeenSwiped: false,
         hasBeenLiked: false
